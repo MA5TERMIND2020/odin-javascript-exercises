1cb92b8345e9f5d7b55a7540436b3e294247614f
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2022-12-25 08:20:55 -0500

    Solved Exercise: 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num < 0) {
+        newString = "ERROR";
+        return newString;
+    }
+    for (i = 0; i < num; i++) {
+        if (string == null) {
+            return newString;
+        }
+        else {
+            newString += string;
+        }
+    }
+    return newString;
 };
 
 // Do not edit below this line
